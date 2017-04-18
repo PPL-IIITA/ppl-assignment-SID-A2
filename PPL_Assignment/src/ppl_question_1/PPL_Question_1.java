3112022cf956670e711222c6baffb21acc2236c1
SID-A2 <siddharthacharaya@gmail.com>
2017-04-19 02:41:07 +0530

    added exception handling

--- PPL_Assignment/src/ppl_question_1/PPL_Question_1.java
+++ PPL_Assignment/src/ppl_question_1/PPL_Question_1.java
@@ -8,7 +8,7 @@ import java.util.List;
 import MAKE_COUPLE.MAKE_COUPLE;
 public class PPL_Question_1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
         List<GIRL> girls ;
         List<BOYS> boys;
         GenerateCsvData.generateBoys();
