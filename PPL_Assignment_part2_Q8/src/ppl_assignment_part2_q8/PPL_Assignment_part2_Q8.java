35fdfc9d31ca50f600ec57ae139134ae5658b99a
SID-A2 <siddharthacharaya@gmail.com>
2017-04-19 03:06:23 +0530

    Update PPL_Assignment_part2_Q8.java

--- PPL_Assignment_part2_Q8/src/ppl_assignment_part2_q8/PPL_Assignment_part2_Q8.java
+++ PPL_Assignment_part2_Q8/src/ppl_assignment_part2_q8/PPL_Assignment_part2_Q8.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 public class PPL_Assignment_part2_Q8 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
         List<GIRL> girls ;
         List<BOYS> boys;
         List<GIFTS> gifts;
